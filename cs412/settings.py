244a1b4e271b4244bca6e5941f9d95e18237f2c1
Ian Campbell <iwc3@bu.edu>
2025-09-12 20:29:43 -0400

    Debugging: removed staticfiles_dir

--- cs412/settings.py
+++ cs412/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' # note: no leading slash!
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, "static"),
+#]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"  # note: no leading slash!
