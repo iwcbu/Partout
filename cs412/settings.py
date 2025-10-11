c474eba21a6399f6b4bb4479f6860d5b35fdfac7
Ian Campbell <iwc3@bu.edu>
2025-10-11 13:14:48 -0400

    finished example videos from blackboard

--- cs412/settings.py
+++ cs412/settings.py
@@ -125,14 +125,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/' # note: no leading slash!
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+# Path for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL= "/media/"  # note: no leading slash!
+MEDIA_URL = '/media/'
 
 import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
--- cs412/urls.py
+++ cs412/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')), # ps2
     path('mini_insta/', include('mini_insta.urls')), # ps3
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
