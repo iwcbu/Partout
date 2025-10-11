5f42145d057e1891369f76677e7e8df328721562
Ian Campbell <iwc3@bu.edu>
2025-10-11 15:28:08 -0400

    implemented many to one relationship

--- cs412/urls.py
+++ cs412/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('mini_insta/', include('mini_insta.urls')), # ps3
 
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
