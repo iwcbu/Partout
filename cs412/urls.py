040208b9418de07df67e636c8bd7900d0d11a9f0
Ian Campbell <iwc3@bu.edu>
2026-03-06 17:13:04 -0500

    Trimmed repo to partout only for deployment

--- cs412/urls.py
+++ cs412/urls.py
@@ -21,17 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("hw/", include("hw.urls")), # module 1 example
-    path("quotes/", include("quotes.urls")), # ps1
-    path("formdata/", include("formdata.urls")), # module 2 example
-    path('restaurant/', include('restaurant.urls')), # ps2
-    path('blog/', include('blog.urls')), # modules 3-7 example
-    path('mini_insta/', include('mini_insta.urls')), # ps3-ps7
-    path('marathon_analysis/', include('marathon_analysis.urls')), # module 8 example
-    path('voter_analytics/', include('voter_analytics.urls')), # ps8
-    path('dadjokes/', include('dadjokes.urls')), # ps10
     path('partout/', include('partout.urls')), # ps10
-
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
