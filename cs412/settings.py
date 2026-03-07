305d2b15195b0b0b1dd59cdce4a0221981fb1342
Ian Campbell <iwc3@bu.edu>
2026-03-07 13:48:03 -0500

    Handling admin account

--- cs412/settings.py
+++ cs412/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
+# os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['*']
 
 
--- partout/apps.py
+++ partout/apps.py
@@ -4,3 +4,21 @@ from django.apps import AppConfig
 class PartoutConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "partout"
+
+    def ready(self):
+        import os
+
+        new_password = os.environ.get("RESET_ADMIN_PASSWORD")
+        if not new_password:
+            return
+
+        try:
+            from django.contrib.auth.models import User
+            user = User.objects.filter(username="admin").first()
+            if user:
+                user.set_password(new_password)
+                user.is_staff = True
+                user.is_superuser = True
+                user.save()
+        except Exception:
+            pass
