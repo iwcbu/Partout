e70bcf7eabe83fb5bed35301646bf86de04d9383
Ian Campbell <iwc3@bu.edu>
2026-03-10 10:29:18 -0400

    debug cs412/urls.py

--- cs412/urls.py
+++ cs412/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path("partout/", include("partout.urls")),
 ]
 
-urlpatterns += [ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
