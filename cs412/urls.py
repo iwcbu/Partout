80d33226f1f297445c8a56fc9a63cd009f457fa7
Ian Campbell <iwc3@bu.edu>
2026-04-19 13:31:07 -0400

    Add media file serving to URL patterns
    Added a re_path for serving media files from the MEDIA_ROOT.

--- cs412/urls.py
+++ cs412/urls.py
@@ -3,11 +3,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
+from django.views.static import 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("partout/", include("partout.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
