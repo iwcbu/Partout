65d8e3c088ef5c3d4a298157108dd1a59ea3c00a
Ian Campbell <iwc3@bu.edu>
2026-04-19 13:41:22 -0400

    Add re_path import to urls.py

--- cs412/urls.py
+++ cs412/urls.py
@@ -2,7 +2,7 @@
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
 
 urlpatterns = [
